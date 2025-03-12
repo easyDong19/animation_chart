e324ef73712e0ef8f0eaad2d2f4407316e0dcb4f
easydong19 <ymh1353@naver.com>
2025-03-12 18:30:47 +0900

    feat: button event 추가

--- src/component/dyChart/component/header/Button.tsx
+++ src/component/dyChart/component/header/Button.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 export const Button = ({ field }: { field: any }) => {
   return (
     <button
+      onClick={field.clickEvent}
       className={classNames(
         {
           'border-gray-800 border flex items-center gap-[0.375rem] px-3 py-[0.375rem] rounded-[0.25rem]':
