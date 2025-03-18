69606790f2892a3c1317195c5f26e405268d0bdc
easydong19 <ymh1353@naver.com>
2025-03-18 10:39:29 +0900

    chore: three.js 환경 세팅

--- src/appRouter.tsx
+++ src/appRouter.tsx
@@ -7,8 +7,6 @@ import {
 import Layout from './Layout';
 import ProtoTypeChart from './ProtoTypeChart';
 import { TestChart } from './component/chart/test/TestChart';
-import { TestHeatmap } from './component/chart/test/TestHeatmap';
-import { TestBPMChart } from './component/chart/test/TestBPMChart';
 import XAxisMovingChart from './component/chart/test/TmpTestChart';
 import HeatmapRChart from './component/chart/test/TestHeatmapR';
 import DyChartPage from './DyChartPage';
@@ -19,8 +17,6 @@ export const AppRouter = () => {
     <Route path='/' element={<Layout />}>
       <Route index element={<ProtoTypeChart />} />
       <Route path='/test' element={<TestChart />} />
-      <Route path='/heatmaptest' element={<TestHeatmap />} />
-      <Route path='/bpmtest' element={<TestBPMChart />} />
       <Route path='/experiment' element={<XAxisMovingChart />} />
       <Route path='/experiment2' element={<HeatmapRChart />} />
       <Route path='/dychart' element={<DyChartPage />} />
