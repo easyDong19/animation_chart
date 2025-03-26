a681b0c7c04f1eda32875506ef9279ed26afa021
easydong19 <ymh1353@naver.com>
2025-03-26 15:06:06 +0900

    feat: 색깔이 변경되는 egg 모델 추가

--- src/ThreePage.tsx
+++ src/ThreePage.tsx
@@ -1,14 +1,15 @@
-import { EggComponent } from '@/EggComponent';
-import { EggObjComponent, EggTest } from '@/EggObjComponent';
+import { EggTest } from '@/EggObjComponent';
 import { Canvas } from '@react-three/fiber';
 
 export const ThreePage = () => {
   return (
     <div>
       <div className='w-[800px] h-[800px] bg-gray-400'>
-        <Canvas gl={{ alpha: true }} style={{ background: 'transparent' }}>
-          {/* <EggComponent /> */}
-          {/* <EggObjComponent /> */}
+        <Canvas
+          gl={{ alpha: true }}
+          style={{ background: 'transparent' }}
+          camera={{ position: [100, 100, 0] }}
+        >
           <EggTest />
         </Canvas>
       </div>
