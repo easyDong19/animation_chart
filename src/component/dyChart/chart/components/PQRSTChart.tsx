20728c6eced52167664636e62734819a5b7b5029
easydong19 <ymh1353@naver.com>
2025-03-27 15:36:43 +0900

    feat: PQRST metaInfo 추가

--- src/component/dyChart/chart/components/PQRSTChart.tsx
+++ src/component/dyChart/chart/components/PQRSTChart.tsx
@@ -18,6 +18,18 @@ type MetaInfoType = {
   [key: string]: number;
 };
 
+const metaInfoColor = {
+  QRS_start: 'blue',
+  QRS_end: 'blue',
+  R_peak: 'green',
+  T_end: 'red',
+  T_peak: 'red',
+  T_start: 'red',
+  P_start: 'orange',
+  P_end: 'orange',
+  P_peak: 'orange',
+};
+
 const generateColorPalette = (count: number): string[] => {
   const colors: string[] = [];
   for (let i = 0; i < count; i++) {
@@ -29,6 +41,27 @@ const generateColorPalette = (count: number): string[] => {
 
 const createMetaInfo = (metaData: MetaInfoType) => {
   console.log(metaData);
+  let metaDataList = [];
+  metaDataList = Object.entries(metaData).map(([key, x]) => {
+    return {
+      x: [x, x],
+      y: [-1, 1],
+      type: 'scatter',
+      mode: 'lines+markers',
+      line: {
+        color: metaInfoColor[key],
+        width: 1,
+      },
+      marker: {
+        size: 1,
+        opacity: 0,
+      },
+      hoverinfo: 'text',
+      text: [key, key],
+    };
+  });
+
+  return metaDataList;
 };
 
 const createPQRSTData = (
@@ -41,51 +74,35 @@ const createPQRSTData = (
 
   // PQRST 포인트 기록
   const metaInfo = originData[metaInfoKey];
+  const metaInfoList = createMetaInfo(metaInfo);
 
   const x = Array.from({ length: timeSeriesLength }, (_, i) => i);
   const colors = generateColorPalette(series.length);
 
-  return series.map((row: number[], idx: number) => ({
+  const data = series.map((row: number[], idx: number) => ({
     x,
     y: row,
     type: 'scatter',
     mode: 'lines',
     line: {
       color: colors[idx],
-      size: 1,
+      size: 0.5,
     },
   }));
+
+  return data.concat(metaInfoList);
 };
 
 export const PQRSTChart = () => {
   const pqrstData = useMemo(() => createPQRSTData(Rawdata), []);
 
-  const verticalLineTrace: TraceDataFormat = {
-    x: [50, 50],
-    y: [-1, 1],
-    type: 'scatter',
-    mode: 'lines+markers',
-    line: {
-      color: 'red',
-      width: 2,
-    },
-    marker: {
-      size: 5,
-      color: 'red',
-      symbol: 'circle',
-      opacity: 0,
-    },
-    hoverinfo: 'text',
-    text: ['PQRST Key Point'],
-    showlegend: false,
-  };
-
   return (
     <div className='w-full h-[400px]'>
       <Plot
-        data={pqrstData.concat(verticalLineTrace)}
+        data={pqrstData}
         layout={{
           showlegend: false,
+          xaxis: { showgrid: false },
         }}
         style={{ width: '100%', height: '100%' }}
         config={{
